4b18f9c9a739664e109d85d7db637d400d6d1369
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-19 04:25:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,19 +3,18 @@ def prime? (integer)
   #Handle the only even prime
   if integer == 2
     true
-  #eliminate less than 2 and even numbers
+  #eliminate less than 2 and other even numbers
   elsif integer < 2 || integer % 2 == 0
     false
   else
-    #create list of numbers to check
     now_testing = 3
     while now_testing < integer / 2
       if integer % now_testing == 0
-        false
-      else now_testing += 2
+        return false
+      else 
+        now_testing += 2
       end
     end
-
-    true
+    return true
   end
 end
