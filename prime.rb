ea3df55d3478d019aed7f4d1923d93cf1d5715dc
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-19 04:02:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
 # Add  code here!
 def prime? (integer)
-
-  #eliminate less than 2 and even numbers
-  if integer < 2 || integer % 2 == 0
-    false
   #Handle the only even prime
-  elsif integer == 2
+  if integer == 2
     true
+  #eliminate less than 2 and even numbers
+  elsif integer < 2 || integer % 2 == 0
+    false
   else
     #create list of numbers to check
     active_number = 3
